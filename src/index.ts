7b04d8bff294a47df4efd510134f7ffc9654690f
Pofay <gianCarlo.gilos@gmail.com>
2022-11-30 19:22:00 +0800

    Update development environment settings

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const TOKEN = process.env.DISCORD_TOKEN || ''
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`)
+})
+
+client.login(TOKEN).catch(error => console.error(error))
+client.destroy()
