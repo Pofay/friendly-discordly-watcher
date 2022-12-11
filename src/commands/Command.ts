327b9fcb8ac45d48f926b00c6ffe2d895af7db81
Pofay <gianCarlo.gilos@gmail.com>
2022-12-11 12:47:00 +0800

    Changed type to return any due to looseness of types being return from Discord commands

--- src/commands/Command.ts
+++ src/commands/Command.ts
@@ -1,8 +1,13 @@
-import { ChatInputApplicationCommandData, Client, CommandInteraction, Message } from "discord.js";
-import { Hello } from "./hello";
+import {
+	ChatInputApplicationCommandData,
+	Client,
+	CommandInteraction,
+} from 'discord.js'
+import { Hello } from './hello'
+import { BotSetup } from './bot-setup'
 
 export interface Command extends ChatInputApplicationCommandData {
-    run: (client: Client, interaction: CommandInteraction) => Promise<Message<boolean>>
+	run: (client: Client, interaction: CommandInteraction) => any
 }
 
-export const Commands: Command[] = [Hello]
+export const Commands: Command[] = [Hello, BotSetup]
