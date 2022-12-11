21f755292850858c6685e8edbf2e492047a8cdce
Pofay <gianCarlo.gilos@gmail.com>
2022-12-11 12:46:22 +0800

    Removed unused commands

--- src/commands/user.ts
+++
@@ -1,8 +0,0 @@
-import { CommandInteraction, SlashCommandBuilder } from 'discord.js'
-
-export const data = new SlashCommandBuilder()
-	.setName('user')
-	.setDescription('Provides info about User')
-
-export const execute = async (interaction: CommandInteraction) =>
-	await interaction.reply(`Command was run by ${interaction.user.username}`)
