e9c163a30cb888b3f1524e1c56d3f4123cb7a26b
Pofay <gianCarlo.gilos@gmail.com>
2023-01-12 06:56:51 +0000

    Used discordjs v14 style of deployment and command handling

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,1 +1,2 @@
 export * as forwardLink from './forward-link'
+export * as forwardLinks from './forward-links'
--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,20 +1,28 @@
-import { Commands, Command } from 'commands/Command'
-import { REST, Routes, SlashCommandBuilder } from 'discord.js'
+import * as commandModules from 'commands'
+import { REST, Routes } from 'discord.js'
 import * as dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 const TOKEN = process.env.DISCORD_TOKEN ?? ''
 const CLIENT_ID = process.env.CLIENT_ID ?? ''
 const GUILD_ID = process.env.GUILD_ID ?? ''
 
-const slashCommands = Commands.map((c: Command) =>
-	new SlashCommandBuilder().setName(c.name).setDescription(c.description)
-)
+type Command = {
+	data: unknown
+}
+
+const slashCommands = []
+
+for (const module of Object.values<Command>(commandModules)) {
+	slashCommands.push(module.data)
+}
 
 const rest = new REST({ version: '10' }).setToken(TOKEN)
 
 rest
-	.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: slashCommands })
+	.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+		body: slashCommands,
+	})
 	.then(() => console.log('Successfully Registered application commands'))
 	.catch(console.error)
